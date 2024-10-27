b23910727d4ea89fe6801b1444108a476e820d37
Kyle-Inc-Woo <143403168+Kyle-Inc-Woo@users.noreply.github.com>
2024-10-26 22:08:27 -0700

    NumberGuessGame-10/26/2024

---
+++ NumberGuessGame/NumberGuess.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberGuessGame
+{
+    internal class NumberGuess
+    {
+        private int num;
+        public int IsEven { get; set; }
+        public bool IsBigger { get; private set; }
+
+        public NumberGuess()
+        {
+           Generate();
+        }
+
+        public void Generate()
+        {
+            Random rnd = new Random();
+            num = rnd.Next(1,11);
+            IsEven = num % 2;
+        }
+
+        public int GetNum()
+        {
+            return num;
+        }
+
+        public void Compare(int userInput)
+        {
+            IsBigger = userInput < num;
+        }
+    }
+}
