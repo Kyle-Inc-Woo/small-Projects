1b2a5e78962c7ca6f73748b6789f33bef37aa5da
Inchang Woo <143403168+Kyle-Inc-Woo@users.noreply.github.com>
2024-10-28 20:12:51 -0700

    ArrayListDemo-10/28/2024

---
+++ ArrayListDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+
+namespace RealArrayListDemo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ArrayList myArrayList = new ArrayList();
+        
+        myArrayList.Add(1);
+        myArrayList.Add(2.54);
+        myArrayList.Add("Hello world");
+        
+        //myArrayList.Remove(1);   --> Remove
+        //myArrayList.RemoveAt(1); --> Remove in terms of index
+        //Console.WriteLine(myArrayList[2]);
+        
+        // Know about how many elements are there.
+        // Console.WriteLine(myArrayList.Count);
+        
+        
+        for (int i = 0; i < myArrayList.Count; i++)
+        {
+            Console.WriteLine(myArrayList[i]);
+        }
+        
+        
+        // We can't use it.
+        //foreach (int num in myArrayList)
+        //{
+        //   Console.WriteLine(num);
+        //}
+        
+        Console.ReadKey();
+    }
+}
